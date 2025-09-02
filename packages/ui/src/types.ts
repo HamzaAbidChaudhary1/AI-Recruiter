9bae807da26cf5f0f907f3488f815bfd0ec2efa5
HamzaAbidChaudhary1 <hamzaabid455@gmail.com>
2025-09-02 19:58:42 +0500

    Create types.ts

---
+++ packages/ui/src/types.ts
@@ -0,0 +1,10 @@
+export interface CardProps {
+  variant?: 'default' | 'elevated' | 'outlined'
+  className?: string
+}
+
+export interface AnimationProps {
+  duration?: number
+  delay?: number
+  ease?: string
+}
